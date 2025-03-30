b6f2bbb8e8363e1448b1ac29e1b791018e5ec6b2
electricalengineer173 <electricalengineer173@example.com>
2025-03-30 11:02:13 +0500

    Add middleware in main.py

--- dynamictool/main.py
+++ dynamictool/main.py
@@ -38,7 +38,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000","https://gas-analyze.vercel.app/"],
     #allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
